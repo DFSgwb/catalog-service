21fad5b60fab35a823dd58a58ff5170460f11208
DFSgwb <2973024690@qq.com>
2023-08-24 22:03:49 +0800

    update

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,11 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+// 在Spring上下文中加载配置数据bean
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
